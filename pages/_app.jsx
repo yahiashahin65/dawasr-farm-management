3b71ba0918b73521a32f2b8851af71b35412d79b
Yahia Shahin <92226321+yahiashahin65@users.noreply.github.com>
2026-05-25 10:16:48 +0300

    Wrap Component with AuthProvider for context

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { AuthProvider } from "../context/AuthContext";
+
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
