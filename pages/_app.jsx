efb9e2b04f31ba984dda28cd75bfadbf0815ef26
Yahia Shahin <92226321+yahiashahin65@users.noreply.github.com>
2026-05-31 13:21:25 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
+
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+
 import { AuthProvider } from "../context/AuthContext";
 
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (
+      "serviceWorker" in navigator &&
+      process.env.NODE_ENV === "production"
+    ) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js").catch(console.error);
+      });
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
